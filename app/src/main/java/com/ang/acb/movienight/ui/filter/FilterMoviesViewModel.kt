b86e057fdbd76d9fa7ebb86b6c79d3b35ba806c2
Angela Aciobanitei <aciobanitei.angela@gmail.com>
2021-06-17 11:52:27 +0100

    delete unused methods in filter view model

--- app/src/main/java/com/ang/acb/movienight/ui/filter/FilterMoviesViewModel.kt
+++ app/src/main/java/com/ang/acb/movienight/ui/filter/FilterMoviesViewModel.kt
@@ -1,27 +1,20 @@
 package com.ang.acb.movienight.ui.filter
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.ang.acb.movienight.domain.entities.Movie
 import com.ang.acb.movienight.domain.entities.MovieFilter
 import com.ang.acb.movienight.domain.usecases.GetFilteredMoviesUseCase
-import com.ang.acb.movienight.domain.usecases.GetMovieDetailsUseCase
-import com.ang.acb.movienight.domain.usecases.SaveFavoriteMovieUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class FilterMoviesViewModel @Inject constructor(
     private val getFilteredMoviesUseCase: GetFilteredMoviesUseCase,
-    private val saveFavoriteMovieUseCase: SaveFavoriteMovieUseCase,
-    private val getMovieDetailsUseCase: GetMovieDetailsUseCase,
 ) : ViewModel() {
 
     @FlowPreview
@@ -35,21 +28,5 @@ class FilterMoviesViewModel @Inject constructor(
                 )
             }
         ).flow
-    }
-
-    // TODO This method is here for testing purposes
-    fun saveFavoriteMovie(movie: Movie) {
-        viewModelScope.launch {
-            val id = saveFavoriteMovieUseCase(movie)
-            Timber.d("asd saved to db movie with id=$id")
-        }
-    }
-
-    // TODO This method is here for testing purposes
-    fun getMovieDetails(movieId: Long) {
-        viewModelScope.launch {
-            val movie = getMovieDetailsUseCase(movieId)
-            Timber.d("asd movie=$movie")
-        }
     }
 }
